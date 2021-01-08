4ce1e09cb3b136821620ef1708ba849b2fa229e0
Noah Lee <17ngml@caslab.queensu.ca>
2021-01-07 22:16:06 -0500

    player can move along path

--- FindThePath/FindThePath/GameScene.swift
+++ FindThePath/FindThePath/GameScene.swift
@@ -15,50 +15,108 @@ class GameScene: SKScene {
  //   private var spinnyNode : SKShapeNode?
     var numRows = 9
     var numCols = 5
-    var arr:[[SKSpriteNode]]?// = [[SKSpriteNode]](repeating: [SKSpriteNode](repeating: SKSpriteNode.init(), count: numCols), count: numRows)
+    var curRow:Int!
+    var curCol:Int!
+    var matrix:TileMatrix!
+    var Player:SKSpriteNode!
+    var blockSize:CGFloat!
     let screenSize = UIScreen.main.bounds
     
     override func didMove(to view: SKView) {
-        if let grid = Grid(blockSize: screenSize.height/CGFloat(numCols), rows:numRows, cols:numCols) {
-            grid.position = CGPoint (x:frame.midX, y:frame.midY)
-            addChild(grid)
-            makeArray()
-            for row in 0..<numRows {
-                if row == 0 || row == 8 {
-                    for col in 0..<numCols {
-                        let platform = SKSpriteNode(imageNamed: "WoodPlatform")
-                        platform.setScale(grid.blockSize/64.0)
-                        platform.position = grid.gridPosition(row:row, col:col)
-                        grid.addChild(platform)
-                        arr![row][col] = platform
-                    }
-                }
-                else {
-                    for col in 0..<numCols {
-                        let tile = SKSpriteNode(imageNamed: "Tile")
-                        tile.setScale(grid.blockSize/64.0 * 0.75)
-                        tile.position = grid.gridPosition(row: row, col: col)
-                        grid.addChild(tile)
-                        arr![row][col] = tile
-                    }
+        matrix = TileMatrix(rows: numRows, cols: numCols)
+        blockSize = screenSize.height/CGFloat(numCols)
+        for row in 0..<numRows {
+            if row == 0 || row == 8 {
+                for col in 0..<numCols {
+                    let platform = SKSpriteNode(imageNamed: "WoodPlatform")
+                    platform.setScale(blockSize/64.0)
+                    platform.position = gridPosition(blockSize: blockSize, row:row, col:col)
+                    platform.zPosition = 2.0
+                    self.addChild(platform)
+                    matrix.arr[row][col].sprite = platform
                 }
             }
-     
-            let path = PathArray(rows: numRows, cols: numCols)
-            path.createPath()
-            for row in 1..<numRows-1 {
-                for col in 0..<numCols{
-                    if path.arr![row][col].isPath == true{
-                        arr![row][col].color = .red
-                        arr![row][col].colorBlendFactor = 1
-                    }
+            else {
+                for col in 0..<numCols {
+                    let tile = SKSpriteNode(imageNamed: "Tile")
+                    tile.setScale(blockSize/64.0 * 0.9)
+                    tile.position = gridPosition(blockSize: blockSize, row: row, col: col)
+                    tile.zPosition = 2.0
+                    self.addChild(tile)
+                    matrix.arr[row][col].sprite = tile
                 }
             }
         }
+        matrix.createPath()
+        for row in 1..<numRows-1 {
+            for col in 0..<numCols{
+                if matrix.arr[row][col].isPath == true{
+                    matrix.arr[row][col].sprite.color = .red
+                    matrix.arr[row][col].sprite.colorBlendFactor = 1
+                }
+            }
+        }
+        Player = SKSpriteNode(imageNamed: "Player")
+        Player.setScale(blockSize/64.0 * 0.75)
+        Player.position = gridPosition(blockSize: blockSize, row: numRows - 1, col: numCols/2)
+        Player.zPosition = 3.0
+        self.addChild(Player)
+        curRow = numRows - 1
+        curCol = numCols / 2
+        initializeArrow(dir: 0, bs: blockSize)
+        initializeArrow(dir: 1, bs: blockSize)
+        initializeArrow(dir: 2, bs: blockSize)
+        initializeArrow(dir: 3, bs: blockSize)
     }
     
-    func makeArray(){
-        arr = [[SKSpriteNode]](repeating: [SKSpriteNode](repeating: SKSpriteNode(), count: numCols), count: numRows)
+    func gridPosition(blockSize:CGFloat, row:Int, col:Int) -> CGPoint {
+        let offset = blockSize / 2.0 + 0.5
+        let x = CGFloat(col) * blockSize - (blockSize * CGFloat(numCols)) / 2.0 + offset
+        let y = CGFloat(numRows - row - 1) * blockSize - (blockSize * CGFloat(numRows)) / 2.0 + offset
+        return CGPoint(x:x, y:y)
+    }
+    
+    func arrowPosition(xoffset: CGFloat, yoffset: CGFloat, blockSize: CGFloat, row: Int, col: Int) -> CGPoint {
+        let offset = blockSize / 2.0 + 0.5
+        let x = CGFloat(col) * blockSize - (blockSize * CGFloat(numCols)) / 2.0 + offset
+        let y = CGFloat(numRows - row - 1) * blockSize - (blockSize * CGFloat(numRows)) / 2.0 + offset
+        return CGPoint(x:x + xoffset, y:y + yoffset)
+    }
+    
+    func initializeArrow(dir: Int, bs: CGFloat) {
+        var xoffset:CGFloat!
+        var yoffset:CGFloat!
+        var rotation:CGFloat!
+        let arrow = SKSpriteNode(imageNamed: "arrow")
+        switch dir{
+        case 0:
+            xoffset = bs/2.0
+            yoffset = bs
+            rotation = 0
+            arrow.name = "up"
+        case 1:
+            xoffset = bs
+            yoffset = bs/2.0
+            rotation = 3/2
+            arrow.name = "right"
+        case 2:
+            xoffset = 0
+            yoffset = bs/2.0
+            rotation = 1/2
+            arrow.name = "left"
+        case 3:
+            xoffset = bs/2.0
+            yoffset = 0
+            rotation = 1.0
+            arrow.name = "down"
+        default:
+        return
+        }
+        arrow.position = arrowPosition(xoffset: xoffset, yoffset: yoffset, blockSize: bs, row: numRows - 1, col: 0)
+        arrow.zRotation = rotation * CGFloat.pi
+        arrow.setScale(bs/64.0)
+        arrow.zPosition = 3.0
+        self.addChild(arrow)
     }
     
 /*
@@ -69,7 +127,8 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+ */
+    /*
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -85,7 +144,7 @@ class GameScene: SKScene {
             self.addChild(n)
         }
     }
-    
+ 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
@@ -93,7 +152,46 @@ class GameScene: SKScene {
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
+ */
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches {
+            let touchedNode = self.atPoint(t.location(in: self))
+            if touchedNode.name == "up" {
+                Player.position = gridPosition(blockSize: blockSize, row: curRow - 1, col: curCol)
+                curRow = curRow - 1
+            }
+            else if touchedNode.name == "right" {
+                Player.position = gridPosition(blockSize: blockSize, row: curRow, col: curCol + 1)
+                curCol = curCol + 1
+            }
+            else if touchedNode.name == "left" {
+                Player.position = gridPosition(blockSize: blockSize, row: curRow, col: curCol - 1)
+                curCol = curCol - 1
+            }
+            else if touchedNode.name == "down" {
+                Player.position = gridPosition(blockSize: blockSize, row: curRow + 1, col: curCol)
+                curRow = curRow + 1
+            }
+            if matrix.arr[curRow][curCol].isPath == false {
+                Player.position = gridPosition(blockSize: blockSize, row: numRows - 1, col: numCols/2)
+                curRow = numRows - 1
+                curCol = numCols/2
+            }
+        }
+        /*
+        let touchedNode = self.nodeAtPoint(positionInScene)
+        
+        if let name = touchedNode.name
+        {
+            if name == "pineapple"
+            {
+                print("Touched")
+            }
+        }
+        */
+    }
+  /*
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
--- FindThePath/FindThePath/TileMatrix.swift
+++ FindThePath/FindThePath/TileMatrix.swift
@@ -1,8 +1,8 @@
 //
-//  PathArray.swift
+//  TileMatrix.swift
 //  FindThePath
 //
-//  Created by Noah Lee on 2021-01-06.
+//  Created by Noah Lee on 2021-01-07.
 //  Copyright © 2021 NoahAle. All rights reserved.
 //
 
@@ -11,39 +11,40 @@ import SpriteKit
 struct Tile {
     var isPath = false
     var pathIndex = 0
+    var sprite:SKSpriteNode!
 }
 
-class PathArray{
+class TileMatrix{
     
-    var numRows:Int?
-    var numCols:Int?
-    var arr:[[Tile]]?
+    var numRows:Int!
+    var numCols:Int!
+    var arr:[[Tile]]!
     
     convenience init(rows: Int, cols: Int){
         self.init()
         self.numRows = rows
         self.numCols = cols
-        self.arr = [[Tile]](repeating: [Tile](repeating: Tile(isPath: false, pathIndex: 0), count: numCols!), count: numRows!)
+        self.arr = [[Tile]](repeating: [Tile](repeating: Tile(isPath: false, pathIndex: 0, sprite: SKSpriteNode.init()), count: numCols), count: numRows)
     }
     
     func createPath() {
-        let startIndex = Int(arc4random_uniform(UInt32(numCols!)))
-        var curRow = numRows! - 2
+        let startIndex = Int(arc4random_uniform(UInt32(numCols)))
+        var curRow = numRows - 2
         var curColumn = startIndex
         var curPathIndex = 1
-        for i in 0..<(numCols!) {
-            arr![0][i].isPath = true
-            arr![numRows! - 1][i].isPath = true
+        for i in 0..<(numCols) {
+            arr[0][i].isPath = true
+            arr[numRows - 1][i].isPath = true
         }
         
-        arr![curRow][startIndex].isPath = true
-        arr![curRow][startIndex].pathIndex = curPathIndex
+        arr[curRow][startIndex].isPath = true
+        arr[curRow][startIndex].pathIndex = curPathIndex
         
         while curRow > 1 {
             let dir = Int(arc4random_uniform(3)) //0 = left, 1 = up, 2 = right
             if dir == 0 {
                 if curColumn > 0 {
-                    if !arr![curRow][curColumn - 1].isPath {
+                    if !arr[curRow][curColumn - 1].isPath {
                         curColumn -= 1
                         curPathIndex += 1
                     }
@@ -51,15 +52,15 @@ class PathArray{
             }
             if dir == 1 {
                 if curRow > 0 {
-                    if !arr![curRow - 1][curColumn].isPath {
+                    if !arr[curRow - 1][curColumn].isPath {
                         curRow -= 1
                         curPathIndex += 1
                     }
                 }
             }
             if dir == 2 {
-                if curColumn < numCols! - 1 {
-                    if !arr![curRow][curColumn + 1].isPath {
+                if curColumn < numCols - 1 {
+                    if !arr[curRow][curColumn + 1].isPath {
                         curColumn += 1
                         curPathIndex += 1
                     }
@@ -68,7 +69,7 @@ class PathArray{
             arr![curRow][curColumn].isPath = true
             arr![curRow][curColumn].pathIndex = curPathIndex
         }
-        for (count, row) in arr!.enumerated(){
+        for (count, row) in arr.enumerated(){
             for val in row {
                 print(val.isPath, terminator:"")
                 print(val.pathIndex, terminator: " ")
